a50484d26c120d8dbc3f87f96b0f8a380069ddb2
Logan Garay <garay.logan@gmail.com>
2020-01-23 16:32:54 -0700

    functioning rps game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace testing
 {
@@ -6,7 +7,55 @@ namespace testing
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World! It's me a Logan.");
+      List<string> CpuChoices = new List<string>()
+        {
+            "rock",
+            "paper",
+            "scissors"
+        };
+      Random rnd = new Random();
+      bool playing = true;
+      Console.Clear();
+      Console.WriteLine("Welcome, what is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Hello {name}! My name is Mr. Roboto and I am your opponent today.");
+      Console.WriteLine("Are you ready to play a game? What game you might ask, why only the bestest game in the whole world; Rock, Paper, Scissors! Once we start playing, type anything other than 'rock', 'paper', or 'scissors' and it will exit the game.");
+      Console.WriteLine("Once you're ready, press the SpaceBar.");
+      ConsoleKeyInfo SecondKey = Console.ReadKey();
+      if (SecondKey.Key == ConsoleKey.Spacebar)
+      {
+        while (playing)
+        {
+          Console.WriteLine("What is your move?");
+          string PlayerChoice = Console.ReadLine();
+          int ComputerChoice = rnd.Next(CpuChoices.Count);
+          if (PlayerChoice == CpuChoices[ComputerChoice])
+          {
+            Console.WriteLine("We tied!");
+          }
+          else if ((PlayerChoice == "rock" && CpuChoices[ComputerChoice] == "scissors") || (PlayerChoice == "paper" && CpuChoices[ComputerChoice] == "rock") || (PlayerChoice == "scissors" && CpuChoices[ComputerChoice] == "paper"))
+          {
+            Console.WriteLine("You must have cheated...");
+          }
+          else if ((PlayerChoice == "rock" && CpuChoices[ComputerChoice] == "paper") || (PlayerChoice == "paper" && CpuChoices[ComputerChoice] == "scissors") || (PlayerChoice == "scissors" && CpuChoices[ComputerChoice] == "rock"))
+          {
+            Console.WriteLine("You need to practice more.");
+          }
+          else
+          {
+            Console.Clear();
+            playing = false;
+          }
+        }
+      }
+      else
+      {
+        Console.Clear();
+        Console.WriteLine("You scared?");
+      }
+
+
+
     }
   }
 }
